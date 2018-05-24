bcd4078335bc1c98de87d05d7d6b2bb2efb2387a
Sandeep Sihari <kumarsandeep91@gmail.com>
2018-05-24 16:19:31 +0530

    js array demo

---
+++ scripts.js
@@ -0,0 +1,49 @@
+let myPicturesArray = [
+    {
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "http://placehold.it/600/92c952",
+    "thumbnailUrl": "http://placehold.it/150/92c952"
+    },
+    {
+    "albumId": 1,
+    "id": 2,
+    "title": "reprehenderit est deserunt velit ipsam",
+    "url": "http://placehold.it/600/771796",
+    "thumbnailUrl": "http://placehold.it/150/771796"
+    },
+    {
+    "albumId": 2,
+    "id": 51,
+    "title": "non sunt voluptatem placeat consequuntur rem incidunt",
+    "url": "http://placehold.it/600/8e973b",
+    "thumbnailUrl": "http://placehold.it/150/8e973b"
+    },
+    {
+    "albumId": 2,
+    "id": 52,
+    "title": "eveniet pariatur quia nobis reiciendis laboriosam ea",
+    "url": "http://placehold.it/600/121fa4",
+    "thumbnailUrl": "http://placehold.it/150/121fa4"
+    },
+    {
+    "albumId": 3,
+    "id": 127,
+    "title": "magnam quia sed aspernatur",
+    "url": "http://placehold.it/600/74456b",
+    "thumbnailUrl": "http://placehold.it/150/74456b"
+    },
+    {
+    "albumId": 3,
+    "id": 128,
+    "title": "est facere ut nam repellat numquam quia quia eos",
+    "url": "http://placehold.it/600/b0931d",
+    "thumbnailUrl": "http://placehold.it/150/b0931d"
+    }
+];
+    
+// Display pictures
+// TODO: You need to loop through the above picture Array and append each picture in the body of HTML page. 
+// you should check out document.append(image object) for more information 
+// Your code here
